210cd955462ae262582ae04137e72a4e2884e239
Hui Xu <hui.xu@me.com>
2025-11-11 15:44:19 +0800

    Add species field to ContentState struct
    - Add optional species field to track pet species in live activity
    - Enables dynamic emoji display based on pet type (cat/dog)
    - Supports bilingual species values (English and Chinese)

--- ios-files/LiveActivityWidget.swift
+++ ios-files/LiveActivityWidget.swift
@@ -12,6 +12,7 @@ struct LiveActivityAttributes: ActivityAttributes {
     var progress: Double?
     var imageName: String?
     var dynamicIslandImageName: String?
+    var species: String?
   }
 
   var name: String
